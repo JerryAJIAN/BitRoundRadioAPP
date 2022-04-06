f6bc0ca5043c258ec333e62525cd1e8375fa9dc1
ASUS_JAJIAN <2016928619@qq.com>
2022-04-06 14:16:18 +0800

    提交MainActivity

--- app/src/main/java/com/example/bitroundradio/Activity/SplashActivity.java
+++ app/src/main/java/com/example/bitroundradio/Activity/SplashActivity.java
@@ -7,5 +7,17 @@ public class SplashActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                //主线程中执行
+                startMainActivity();
+            }
+        },2000);
+
+    }
+    private void startMainActivity(){
+        Intent intent = new Intent(this, SplashActivity.this,MainActivity.class);
     }
 }
