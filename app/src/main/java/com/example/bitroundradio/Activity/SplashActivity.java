91d53a406c83433baea99aa1d8c1fc7e8f3f5a04
ASUS_JAJIAN <2016928619@qq.com>
2022-04-06 13:49:07 +0800

    SplashActivity页面

---
+++ app/src/main/java/com/example/bitroundradio/Activity/SplashActivity.java
@@ -0,0 +1,11 @@
+package com.example.bitroundradio.Activity;
+
+
+public class SplashActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
